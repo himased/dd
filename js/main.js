a5b17d0731e62ea6c48c1fa7c0750b8aefc78a4c
himased1214 <chanakan_c@elearning.cmu.ac.th>
2019-12-17 16:26:02 +0700

    go

---
+++ js/main.js
@@ -0,0 +1,11 @@
+console.log('Really??')
+
+        let str ='';
+        for(let i = 0;i<10;i++){
+            console.log(i+1);
+            str += (i+1) + ''
+        }
+
+        console.log(str)
+
+        document.querySelector('#hello').innerHTML = str;
